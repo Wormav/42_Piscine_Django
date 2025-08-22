109c31d49cdb609bd8d081467c631477a3c00966
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-08-22 21:51:50 +0200

    add translate

--- Django_03_2/chessarticles/urls.py
+++ Django_03_2/chessarticles/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("articles.urls")),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("", RedirectView.as_view(url="/en/", permanent=False)),
 ]
+
+urlpatterns += i18n_patterns(
+    path("", include("articles.urls")), prefix_default_language=True
+)
